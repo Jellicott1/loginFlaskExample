5984c185d52a44af3ec0ef7f91ea763c8561bb30
James Ellicott <jellicott@kennetschool.co.uk>
2026-04-27 13:07:15 +0100

    feat: Added singup functionality

--- main.js
+++ main.js
@@ -19,3 +19,25 @@ async function login() {
     alert(error.message);
   }
 }
+
+async function signup() {
+  const username = document.getElementById("username").value;
+  const password = document.getElementById("password").value;
+  try {
+    const response = await fetch("http://127.0.0.1:5000/signup", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ username: username, password: password }),
+    });
+    const result = await response.json();
+    if (result.status === "success") {
+      alert("Successfully Signed Up!");
+    } else {
+      alert("Failed to sign up");
+    }
+  } catch (error) {
+    alert(error.message);
+  }
+}
--- server.py
+++ server.py
@@ -4,6 +4,25 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
+users = []
+with open('loginDetails.txt') as f:
+    line = f.readline().split(',')
+    users.append([line[0],line[1]])
+
+@app.route('/signup', methods=['POST'])
+def signup():
+    #Get the JSON fata from the request
+    data = request.get_json()
+
+    #Check if JSON was provided and contains the required keys
+    if not data or 'username' not in data or 'password' not in data:
+        return jsonify({"status": "failed", "message": "Missing username or password"}), 400
+    
+    users.append([data['username'].strip(),data['password'].strip()])
+    print(users)
+    return jsonify({"status": "success", "message": "Signed up successfully."})
+
+
 @app.route('/login', methods=['POST'])
 def login():
     # Get the JSON data from the request
@@ -14,10 +33,11 @@ def login():
         return jsonify({"status": "failed", "message": "Missing username or password"}), 400
 
     # Validate credentials
-    if data['username'] == "test" and data['password'] == "1234":
-        return jsonify({"status": "success", "message": "Welcome!"}), 200
-    else:
-        return jsonify({"status": "invalid", "message": "Incorrect credentials"}), 401
+    print(users)
+    for i in range(len(users)):
+        if data['username'].strip() == users[i][0] and data['password'].strip() == users[i][1]:
+            return jsonify({"status": "success", "message": "Welcome!"}), 200
+    return jsonify({"status": "invalid", "message": "Incorrect credentials"}), 401
 
 if __name__ == '__main__':
     app.run(debug=True)
